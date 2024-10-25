06fb6495718c5620858239612ffb12bfeb0870a1
ofirbozna <ofirbozn@gmail.com>
2024-10-25 23:52:52 +0300

    Edit onBallClicked() functionality

--- main.js
+++ main.js
@@ -2,7 +2,12 @@
 var gSize = 100
 
 function onBallClick(){
+    if(gSize<400){
     gSize+=50
+    }else{
+        gSize=100
+    }
+
     const elBall = document.querySelector('.ball')
     elBall.style.height = gSize + 'px'
     elBall.style.width = gSize + 'px'
