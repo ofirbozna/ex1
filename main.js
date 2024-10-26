0002239aefdf5df2f6985bb48aaae2d6f2e89cf6
ofirbozna <ofirbozn@gmail.com>
2024-10-26 17:01:23 +0300

    Edit onBallClick() to change ball color

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 'use strict'
 var gSize = 100
 
-function onBallClick(){
-    if(gSize<400){
-    gSize+= getRandomInt(20,60)
-    }else{
-        gSize=100
+function onBallClick() {
+    if (gSize < 400) {
+        gSize += getRandomInt(20, 60)
+    } else {
+        gSize = 100
     }
-
+    var color = getRandomColor()
     const elBall = document.querySelector('.ball')
     elBall.style.height = gSize + 'px'
     elBall.style.width = gSize + 'px'
     elBall.innerText = gSize
+    elBall.style.backgroundColor = color
 }
